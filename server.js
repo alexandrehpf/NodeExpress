93c0a8f68a02b7f0c39903ae241101201586a6c3
Alexandre Ferraz - MAC <alexandrehpf@hotmail.com>
2025-04-23 07:30:00 -0300

    Adicionando as rotas para aplicação

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ import http from 'http';
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "Curso de Express API",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+  };
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Curso de NodeJs');
+  res.end(rotas[req.url]);
 }
 );
+// Inicia o servidor na porta 3000
+// O servidor escuta requisições na porta 3000
 server.listen(PORT, () => {
   console.log('Servidor rodando na porta 3000');
 });
