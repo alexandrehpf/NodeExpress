3bd837c0157df0bc2c8a5ea8e1f11496d82c1a9e
Alexandre Ferraz - MAC <alexandrehpf@hotmail.com>
2025-04-24 10:22:37 -0300

    Adicionado os métodos com o express

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
-import http from 'http';
+// import http from 'http';
+import app from './src/app.js';
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Express API",
-    "/livros": "Entrei na rota livros",
-    "/autores": "Entrei na rota autores"
-  };
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(rotas[req.url]);
-}
-);
 // Inicia o servidor na porta 3000
 // O servidor escuta requisições na porta 3000
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log('Servidor rodando na porta 3000');
 });
