3d87da0a9e466c81b292d2dcb46049559f13f51a
Alexandre Ferraz <alexandrehpf@hotmail.com>
2025-04-25 14:26:35 -0300

    Finalizando o projeto

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js";
 
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
@@ -6,7 +7,8 @@ const livroSchema = new mongoose.Schema({
     autor: { type: String },
     editora: { type: String },
     preco: { type: Number },
-    paginas: { type: Number }
+    paginas: { type: Number },
+    autor: autorSchema
 }, {versionKey: false});
 
 const livro = mongoose.model('livros', livroSchema);
