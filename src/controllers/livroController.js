ba3f19cb7bcea24c3936614920d928c22c40ac05
Alexandre Ferraz - MAC <alexandrehpf@hotmail.com>
2025-04-27 11:57:29 -0300

    Finalizando as tratativas de erro 2

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,5 +1,6 @@
 import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
+import NaoEcontrado from "../erros/NaoEncontrado.js";
 
 class LivroController { 
   static async listarLivros(req, res, next) {
@@ -15,7 +16,12 @@ class LivroController {
     try {
       const id = req.params.id;
       const livroEncontrado = await livro.findById(id);
-      res.status(200).json(livroEncontrado);
+      if (livroEncontrado !== null) {
+        res.status(200).json(livroEncontrado);
+      } else{
+        next(new NaoEcontrado("Id do Livro não localizado."));
+      }
+
     } catch (error) {
       next(error);
     }
@@ -36,8 +42,13 @@ class LivroController {
   static async atualizarLivro(req, res, next) {
     try {
       const id = req.params.id;
-      await livro.findByIdAndUpdate(id, req.body);
-      res.status(200).json({ message: "Livro atualizado com sucesso" });
+      const livroEncontrado = await livro.findByIdAndUpdate(id, req.body);
+      
+      if (livroEncontrado !== null) {
+        res.status(200).json({ message: "Livro atualizado com sucesso" });
+      } else{
+        next(new NaoEcontrado("Id do Livro não localizado."));
+      }
     } catch (error) {
       next(error);
     }
@@ -46,8 +57,13 @@ class LivroController {
   static async deletarLivro(req, res, next) {
     try {
       const id = req.params.id;
-      await livro.findByIdAndDelete(id);
-      res.status(200).json({ message: "Livro excluído com sucesso" });
+      const livroEncontrado = await livro.findByIdAndDelete(id);
+      
+      if (livroEncontrado !== null) {
+        res.status(200).json({ message: "Livro excluído com sucesso" });
+      } else{
+        next(new NaoEcontrado("Id do Livro não localizado."));
+      }
     } catch (error) {
       next(error);
     }
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,12 +3,21 @@ import { autorSchema } from "./Autor.js";
 
 const livroSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId },
-  titulo: { type: String, 
+  titulo: { 
+    type: String, 
     required: [true, "O título do livro é obrigatório"] },
-  editora: { type: String,
-    required: [true, "A editora do livro é obrigatória"] },
-  preco: { type: Number },
-  paginas: { type: Number },
+  editora: { 
+    type: String,
+    required: [true, "A editora do livro é obrigatória"],
+    enum: {
+      values: ["Casa do código", "Alura", "Ficção"],
+      mensage: "A editora {VALUE} não é valida"
+    } 
+  },
+  preco: { 
+    type: Number },
+  numeroPaginas: { 
+    type: Number, min: [10, "O número de páginas deve estár entre 10 e 5000. Valor fornecido {VALUE}"], max: [5000, "O número de páginas deve estár entre 10 e 5000. Valor fornecido {VALUE}"] },
   autor: autorSchema
 }, {versionKey: false});
 
